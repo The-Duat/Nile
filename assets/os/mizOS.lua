9bdd795c45cc4cd4572c3531f1ad7d6b6f49fa5e
Mizosu97 <earthpig97@gmail.com>
2022-12-09 10:14:38 -0500

    bug fixes

--- assets/os/mizOS.lua
+++ assets/os/mizOS.lua
@@ -1,4 +1,4 @@
--m- Made by https://theduat.neocities.org
+-- Made by https://theduat.neocities.org
 
 
 --[=[--[=[--[=[ THIS SCRIPT STORES THE CORE SOURCE CODE OF MIZOS. ONLY EDIT IF YOU KNOW WHAT YOU ARE DOING ]=]--]=]--]=]--
