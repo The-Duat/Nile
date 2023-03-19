33ebfce12999f4c4446f70fe242eb907736dc36c
Mizosu97 <earthpig97@gmail.com>
2023-03-19 18:25:04 -0400

    dev

--- assets/os/testing/mexec.c
+++ assets/os/testing/mexec.c
@@ -3,25 +3,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
+int checkpass(char *givenpass)
+{
+    char truepass[254];
+    FILE *passfile;
+    passfile = fopen("/var/mizOS/security/password");
+    for (int i = 0; (c = fgetc(passfile)) != EOF; i++) {
+        truepass[i] = c;
+    }
+    
+    if (truepass == givenpass) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
 int main(int argc, char *argv[]) {
     int euid = geteuid();
-    char password[254] = "password";
     char arguments[300];
     memset(arguments, 0, sizeof(arguments));
 
-    if (argv[1] != password) {
-        printf("Error - invalid password.");
-        exit(1);
-    }
+    int argstart = 2;
 
-    for (int i = 2; i <= argc; i++) {
-        if (argv[i] != NULL && argv[i] != "(null)") {
-            strcat(arguments, argv[i]);
-            strcat(arguments, " ");
+    if (argv[1] == "exec") {
+        for (int i = 3; i <= argc; i++) {
+            if (argv[i] != NULL && argv[i] != "(null)") {
+                strcat(arguments, argv[i]);
+                strcat(arguments, " ");
+            }
         }
     }
-
-    
 
     printf("EUID: %d\n\nArguments: %s", euid, arguments);
     return 0;
