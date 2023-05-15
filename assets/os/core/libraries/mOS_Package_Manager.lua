4e8e1b5f4e1eeb39f090cbe5566c569d91c3e737
Mizosu97 <earthpig97@gmail.com>
2023-05-15 15:13:21 -0400

    [Dev] Listrepo permissions bug.

--- assets/os/core/libraries/mOS_Package_Manager.lua
+++ assets/os/core/libraries/mOS_Package_Manager.lua
@@ -179,9 +179,7 @@ end
 --[=[ Get list of packages in the Duat's repo ]=]--
 Manager.listRepo = function()
 	say("Downloading package repo.")
-	xs("rm -rf /var/mizOS/repo/* && wget https://entertheduat.org/packages/repo.lua -P /var/mizOS/repo/")
-
-	
+	xs("rm -rf /var/mizOS/repo/* && sudo wget https://entertheduat.org/packages/repo.lua -P /var/mizOS/repo/")
 
 	local mizOSRepo = dofile("/var/mizOS/repo/repo.lua")
 
