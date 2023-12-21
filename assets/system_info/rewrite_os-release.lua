56fa4eb69ce055e82c1690aaf3172a78843d6493
Mizosu97 <earthpig97@gmail.com>
2023-12-21 10:43:53 -0500

    Nile rebrand

--- assets/system_info/rewrite_os-release.lua
+++ assets/system_info/rewrite_os-release.lua
@@ -1,1 +1,38 @@
-sss
+local file = io.open("/etc/os-release", "r")
+local copy = io.open("/etc/nile-os-release", "w")
+
+local splitString = dofile("/var/NileRiver/core/libraries/Nile_Utility_Functions.lua").splitString
+
+if file == nil then
+	print("/etc/os-release not found. Changes not made.")
+	os.exit()
+end
+
+local releaseContents = file:read("*all")
+local newContents = ""
+
+local function addEntry(str)
+	newContents = newContents .. str .. "\n"
+end
+
+
+local relLines = splitString(releaseContents, "\n")
+
+if string.sub(reLines[1], 7, 10) == "NILE" then
+	print("Already overwritten.")
+	os.exit()
+end
+
+for _,line in ipairs(reLines) do
+	local split = splitString(line, "=")
+	local key = split[1]
+	local value = split[2]
+	if key == "NAME" or key == "PRETTY_NAME" then
+		local append = string.sub(value, 2, #value)
+		addEntry(key .. "=" .. "\"Nile - " .. append)
+	elseif key == "LOGO" then
+		addEntry(key .. "=nile-logo\n")
+	else
+		addEntry(key .. "=" .. value .. "\n")
+	end
+end
