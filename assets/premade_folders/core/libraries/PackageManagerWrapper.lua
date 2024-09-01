8ff15a6d4ccb6a3e5d0860f422c76163d963e56c
Mizosu97 <earthpig97@gmail.com>
2024-09-01 02:39:11 -0400

    uwu 2

--- assets/premade_folders/core/libraries/PackageManagerWrapper.lua
+++ assets/premade_folders/core/libraries/PackageManagerWrapper.lua
@@ -27,62 +27,92 @@ local Wrapper = {}
 Wrapper.Install = {}
 Wrapper.Remove = {}
 
+local function create_pipe_and_fork(program, arguments)
+    local read_fd, write_fd = pipe()
+    local read_fd2, write_fd2 = pipe()
 
-Wrapper.Install.pacman = function(packageTable)
     local pid = fork()
+
     if pid == 0 then
+        -- Child process
         dup2(read_fd2, Posix.unistd.STDIN_FILENO)
         dup2(write_fd, Posix.unistd.STDOUT_FILENO)
         dup2(write_fd, Posix.unistd.STDERR_FILENO)
+
         close(read_fd)
         close(write_fd2)
         close(write_fd)
         close(read_fd2)
-        local argTable = {"-S"}
-        for _,p in ipairs(packageTable) do
-            table.insert(argTable, p)
-        end
-        execp("/bin/pacman", argTable)
+
+        execp(program, arguments)
     else
+        -- Parent process
         close(write_fd)
         close(read_fd2)
-        local buffer = ""
-        local capturing = true
-    
-        local function read_output_line_by_line()
-            while capturing do
-                local chunk = Posix.unistd.read(read_fd, 1024)
-                if not chunk or #chunk == 0 then
-                    break
-                end
-                buffer = buffer .. chunk
-                for line in buffer:gmatch("[^\r\n]+") do
-
-                    if string.sub(line, 1, 25) == "error: target not found: " then
-                        capturing = false
-                        close(read_fd)
-                        Fault("The package \"" .. string.sub(line, 26, #line) .. "\" does not exist.")
-                    end
-
-                end
-                buffer = ""
-            end
-        end
-    
-        -- Posix.unistd.write(write_fd2, input)
-    
-        -- Posix.unistd.sleep(1) -- Give some time for the output to be generated
-        read_output_line_by_line()
-    
-        -- Wait for the child process to exit
-        wait(pid)
-        
-        close(read_fd)
-        close(write_fd2)
+        return pid, read_fd, write_fd2
     end
+end
+
+local pid, read_fd, write_fd2
+
+local buffer = ""
+local capturing = true
+
+
+local function send_input(input)
+    Posix.unistd.write(write_fd2, input)
+end
+
+local function switch_to_direct_output()
+    capturing = false
+    close(read_fd)
+    print("Switched to direct output from child process.")
+end
+
+local function switch_back_to_capturing()
+    capturing = true
+    pid, read_fd, write_fd2 = create_pipe_and_fork()
+    print("Switched back to capturing output from child process.")
+end
+
+
+Wrapper.Install.pacman = function(packageTable)
+    pid, read_fd, write_fd2 = create_pipe_and_fork()
+    buffer = ""
+    capturing = true
+    local function read_output_line_by_line()
+        while capturing do
+            local chunk = Posix.unistd.read(read_fd, 1024)
+            if not chunk or #chunk == 0 then
+                break
+            end
+            buffer = buffer .. chunk
+            for line in buffer:gmatch("[^\r\n]+") do
+
+                if string.sub(line, 1, 25) == "error: target not found: " then
+                    switch_to_direct_output()
+                    Fault("The package \"" .. string.sub(line, 26, #line) .. "\" does not exist.")
+                    Exit()
+                end
+
+            end
+            buffer = ""
+        end
+    end
+    read_output_line_by_line()
+    wait(pid)
+    close(write_fd2)
 end
 
 
 
 
 return Wrapper
+
+--[=[
+                    if string.sub(line, 1, 25) == "error: target not found: " then
+                        capturing = false
+                        close(read_fd)
+                        Fault("The package \"" .. string.sub(line, 26, #line) .. "\" does not exist.")
+                    end
+]=]--
