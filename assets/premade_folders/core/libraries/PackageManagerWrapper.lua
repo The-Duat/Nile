e3b5ebbfc6c6a17278d9a911e68be580be0cd074
Mizosu97 <earthpig97@gmail.com>
2024-09-01 02:08:40 -0400

    lanaga

--- assets/premade_folders/core/libraries/PackageManagerWrapper.lua
+++ assets/premade_folders/core/libraries/PackageManagerWrapper.lua
@@ -8,51 +8,7 @@ local wait = Posix.sys.wait.wait
 local read_fd, write_fd = pipe()
 local read_fd2, write_fd2 = pipe()
 
-local function DOTHEFORK()
-    local pid = fork()
-    if pid == 0 then
-        dup2(read_fd2, Posix.unistd.STDIN_FILENO)
-        dup2(write_fd, Posix.unistd.STDOUT_FILENO)
-        dup2(write_fd, Posix.unistd.STDERR_FILENO)
-        close(read_fd)
-        close(write_fd2)
-        close(write_fd)
-        close(read_fd2)
-    
-        execp("/bin/sh", {"/bin/sh"})
-    else
-        close(write_fd)
-        close(read_fd2)
-    
-        local buffer = ""
-    
-        local function read_output_line_by_line()
-            while true do
-                local chunk = Posix.unistd.read(read_fd, 1024)
-                if not chunk or #chunk == 0 then
-                    break
-                end
-                buffer = buffer .. chunk
-                for line in buffer:gmatch("[^\r\n]+") do
-                    print("Output: ", line)
-                end
-                buffer = ""
-            end
-        end
-    
-        -- Posix.unistd.write(write_fd2, input)
-    
-        -- Posix.unistd.sleep(1) -- Give some time for the output to be generated
-        read_output_line_by_line()
-    
-        -- Wait for the child process to exit
-        wait(pid)
-        
-        close(read_fd)
-        close(write_fd2)
-    end
-end
-
+Posix.setenv("LANG", "en_US.UTF-8")
 
 local Wrapper = {}
 Wrapper.Install = {}
@@ -88,7 +44,7 @@ Wrapper.Install.pacman = function(packageTable)
                 buffer = buffer .. chunk
                 for line in buffer:gmatch("[^\r\n]+") do
 
-                    print("Output: ", line)
+                    print("Output: " .. line)
 
                 end
                 buffer = ""
